473f668ba835e19a66e8c8d1dc995d704db41c0a
Sufiyan38 <kingmdarme@gmail.com>
2025-06-15 16:41:04 +0530

    Create keepAlive.js

---
+++ keepAlive.js
@@ -0,0 +1,15 @@
+// keepAlive.js
+const express = require('express');
+const server = express();
+
+server.all('/', (req, res) => {
+  res.send('Bot is alive!');
+});
+
+function keepAlive() {
+  server.listen(3000, () => {
+    console.log('Server is ready.');
+  });
+}
+
+module.exports = keepAlive;
